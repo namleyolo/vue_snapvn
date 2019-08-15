0aa70c336ae2e999775ee711c8b9fccb80361c94
nam <you@example.com>
2019-08-15 18:33:54 +0700

    first commit

---
+++ src/components/index/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+      <p>Yolo</p>
+  </div>
+</template>
+
+<script>
+// import Navbar from './components/Navbar.vue'
+export default {
+  name: 'Index',
+  props: {
+    test: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
